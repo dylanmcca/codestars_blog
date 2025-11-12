341545bfa4d44e93bbb47d10fd9a01216143adae
dylanmcca <dylanmcca@gmail.com>
2025-11-12 20:52:28 +0000

    adding postgres db connections

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_O54cmZRpqvCo@ep-polished-pond-ag9l10ay.c-2.eu-central-1.aws.neon.tech/claim_sweat_blast_946183")
+
